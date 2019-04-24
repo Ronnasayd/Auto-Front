af7c3282fbcceb9c277a4c0b5b9ddf197d7cd115
Ronnasayd Machado <ronnasayd@hotmail.com>
2019-04-24 13:18:51 -0300

    adjust run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
+if [ ! -d "app/static" ]; then
+  mkdir app/static
+  mkdir app/static/src
+  mkdir app/static/src/css
+  mkdir app/static/src/scss
+  mkdir app/static/src/js
+fi
 docker build -t docker-node-front -f node.Dockerfile .
 docker run  -ti  -v "$(pwd):/app" -p 3000:3000 -p 3001:3001 docker-node-front 
