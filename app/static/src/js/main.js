3f4f4272718c8d37e6967d52cc9e096aa7829210
ronnasayd <ronnasayd@hotmail.com>
2020-03-19 13:49:53 -0300

    feat: ajustando erros

--- app/static/src/js/main.js
+++ app/static/src/js/main.js
@@ -2,10 +2,10 @@ const form = document.getElementById("form-subscribe");
 form.onsubmit = e => {
   e.preventDefault();
 };
-
+// teste
 for (let i = 0; i < 100; i++) {
-  element = document.createElement("div");
-  elementSize = `${Math.max(20, Math.random() * 50)}px`;
+  var element = document.createElement("div");
+  var elementSize = `${Math.max(20, Math.random() * 50)}px`;
   element.style.width = elementSize;
   element.style.height = elementSize;
   element.style.background = "rgba(255,255,255,0.3)";
